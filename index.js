e9307470666eeb6b9142a6a9fa9a01d667f3d9b8
CoderRinku <roknusjamanrinku1@gmail.com>
2026-05-22 18:28:54 +0600

    initial commit with express and mongodb setup

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ya5v9cx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const db = client.db('petAdoptionDB');
+        const petsCollection = db.collection('pets');
+        const requestsCollection = db.collection('requests');
+
+        console.log("Connected to MongoDB!");
+    } catch (error) {
+        console.log(error);
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Pet Adoption Server is Running');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
