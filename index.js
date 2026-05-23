cb424837eff70f464b44f04bf58cf14989f50dd5
CoderRinku <roknusjamanrinku1@gmail.com>
2026-05-23 22:36:23 +0600

    feat: add search and category regex filter on GET /pets route

--- index.js
+++ index.js
@@ -1,112 +1,114 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ya5v9cx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
 });
 
 async function run() {
     try {
-        const db = client.db('petAdoptionDB');
-        const petsCollection = db.collection('pets');
-        const requestsCollection = db.collection('requests');
-
+        await client.connect();
         console.log("Connected to MongoDB!");
 
-        app.post('/pets', async (req, res) => {
-            const petData = req.body;
-            const result = await petsCollection.insertOne(petData);
+        const petsCollection = client.db("petAdoptionDB").collection("pets");
+        const requestsCollection = client.db("petAdoptionDB").collection("requests");
+
+        app.get("/", (req, res) => {
+            res.send("Pet Adoption Server is running");
+        });
+
+        app.post("/pets", async (req, res) => {
+            const pet = req.body;
+            const result = await petsCollection.insertOne(pet);
             res.send(result);
         });
 
-        app.get('/pets', async (req, res) => {
-            const cursor = petsCollection.find();
-            const result = await cursor.toArray();
+        app.get("/pets", async (req, res) => {
+            const { search, category } = req.query;
+            const query = {};
+
+            if (search) {
+                query.name = { $regex: search, $options: "i" };
+            }
+
+            if (category) {
+                query.category = { $regex: `^${category}$`, $options: "i" };
+            }
+
+            const result = await petsCollection.find(query).toArray();
             res.send(result);
         });
 
-        app.get('/pets/:id', async (req, res) => {
+        app.get("/my-pets", async (req, res) => {
+            const { email } = req.query;
+            const query = { authorEmail: email };
+            const result = await petsCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get("/pets/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await petsCollection.findOne(query);
             res.send(result);
         });
 
-        app.delete('/pets/:id', async (req, res) => {
+        app.put("/pets/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedPet = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = { $set: updatedPet };
+            const result = await petsCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        app.delete("/pets/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await petsCollection.deleteOne(query);
             res.send(result);
         });
 
-        app.put('/pets/:id', async (req, res) => {
+        app.post("/requests", async (req, res) => {
+            const request = req.body;
+            const result = await requestsCollection.insertOne(request);
+            res.send(result);
+        });
+
+        app.get("/requests", async (req, res) => {
+            const { email } = req.query;
+            const query = email ? { requesterEmail: email } : {};
+            const result = await requestsCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.put("/requests/:id", async (req, res) => {
             const id = req.params.id;
+            const { status } = req.body;
             const filter = { _id: new ObjectId(id) };
-            const options = { upsert: true };
-            const updatedPet = req.body;
-            const pet = {
-                $set: {
-                    name: updatedPet.name,
-                    age: updatedPet.age,
-                    category: updatedPet.category,
-                    location: updatedPet.location,
-                    shortDescription: updatedPet.shortDescription,
-                    longDescription: updatedPet.longDescription,
-                    image: updatedPet.image
-                }
-            };
-            const result = await petsCollection.updateOne(filter, pet, options);
+            const updateDoc = { $set: { status } };
+            const result = await requestsCollection.updateOne(filter, updateDoc);
             res.send(result);
         });
 
-        app.post('/requests', async (req, res) => {
-            const requestData = req.body;
-            const result = await requestsCollection.insertOne(requestData);
-            res.send(result);
-        });
-
-        app.get('/requests', async (req, res) => {
-            const cursor = requestsCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        });
-
-        app.get('/my-pets', async (req, res) => {
-            let query = {};
-            if (req.query?.email) {
-                query = { authorEmail: req.query.email };
-            }
-            const cursor = petsCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
-        });
-
-    } catch (error) {
-        console.log(error);
-    }
+    } finally {}
 }
-run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Pet Adoption Server is Running');
-});
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
